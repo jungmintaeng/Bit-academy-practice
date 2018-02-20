52e1b3e3973fc5a582e35f305a769d30126ab1d3
BIT <BIT@DESKTOP-E6ERE49>
2018-02-20 16:59:40 +0900

    -

--- src/prob2/Prob2.java
+++ src/prob2/Prob2.java
@@ -5,7 +5,6 @@ import java.util.Scanner;
 
 public class Prob2 {
 	private static final int ARRAY_LENGTH = 5;
-	
 	public static void main(String[] args) {
 		Scanner scanner = new Scanner( System.in );
 		System.out.println( ARRAY_LENGTH + "개의 숫자를 입력하세요." );
